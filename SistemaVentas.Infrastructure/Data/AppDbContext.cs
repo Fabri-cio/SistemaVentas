5e197623217a9b0a3638af4ebf742a58d264f780
wil <wil.fabri777@gmail.com>
2026-05-27 15:58:10 -0400

    Se añade AppDbContext, configuración de cadena de conexión en appsettings.json y registro de DbContext en Program.cs.

---
+++ SistemaVentas.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVentas.Infrastructure.Data;
+
+// Clase principal de conexion de la base de datos, hereda de DbContext
+public class AppDbContext : DbContext
+{
+    // Recibe configuracion de Entity Framework Core y SQL Server a traves de inyeccion de dependencias
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
