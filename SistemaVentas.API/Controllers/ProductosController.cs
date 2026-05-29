3e37f01fdef0bbbccf9275c0a5909bec7e0eae8e
wil <wil.fabri777@gmail.com>
2026-05-28 20:25:40 -0400

    Habilitar controladores y Swagger; agregar ProductosController
    Se configuraron servicios para controladores y Swagger en Program.cs, eliminando el endpoint de ejemplo. Se añadió ProductosController con endpoint GET para listar productos desde la base de datos. Se ajustó la versión de Swashbuckle.AspNetCore en el archivo de proyecto.

---
+++ SistemaVentas.API/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaVentas.Infrastructure.Data;
+
+namespace SistemaVentas.API.Controllers
+{
+    // Indica que es un controlador API
+    [ApiController]
+
+    // Ruta base: /api/productos
+    [Route("api/productos")]
+    public class ProductosController : ControllerBase
+    {
+        // Conexion con la bae de datos
+        private readonly AppDbContext _context;
+
+        // Inyeccion de dependencias del contexto de datos (DbContext)
+        public ProductosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Endpoint GET: /api/productos
+        [HttpGet]
+        public async Task<IActionResult> GetProductos()
+        {
+            // Obtiene la lista de productos desde la base de datos de forma asincrona
+            var productos = await _context.Productos.ToListAsync();
+
+            // Retorna la lista de productos con un estado HTTP 200 OK
+            return Ok(productos);
+        }
+
+    }
+}
--- SistemaVentas.API/Program.cs
+++ SistemaVentas.API/Program.cs
@@ -3,31 +3,23 @@ using SistemaVentas.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Agreda soporte para controlllers
+builder.Services.AddControllers();
+
+// Configuracion de Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Conexion con SQL Server LocalDB
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     )
 );
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddOpenApi(); //desactivado porque usaremos swagger (mas estandar en empresas)
-
-// Swagger activo
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); //agregamos el servicio de swagger
-
 var app = builder.Build();
 
-// No usamos openapi 
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.MapOpenApi();
-//}
-
-
-// Swagger activo
+// Swagger solo en desarrollo
 if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();     //genera JSON con la documentación de la API
@@ -36,28 +28,8 @@ if(app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Habilita Controllers
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
