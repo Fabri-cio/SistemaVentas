303ff10b5d93b4caf6479a2861dbbffd04912daf
wil <wil.fabri777@gmail.com>
2026-05-29 18:00:28 -0400

    Añade validaciones de datos a CreateProductoDto
    Se importan las anotaciones de DataAnnotations y se aplican restricciones de validación a las propiedades Nombre, Precio y Stock en el DTO CreateProductoDto para asegurar la integridad de los datos recibidos desde la API.

--- SistemaVentas.Application/DTOs/CreateProductoDto.cs
+++ SistemaVentas.Application/DTOs/CreateProductoDto.cs
@@ -1,10 +1,18 @@
-﻿namespace SistemaVentas.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVentas.Application.DTOs
 {
     // DTO pare crear productos desde la API
     public class CreateProductoDto
     {
+        [Required]
+        [MaxLength(255)]
         public string Nombre { get; set; } = string.Empty;
+
+        [Range(0.01, 999999)]
         public decimal Precio { get; set; }
+
+        [Range(0,100000)]
         public int Stock { get; set; }
     }
 }
