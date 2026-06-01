b0d6fd9eea3c87a4708fd0155fc40807f8c7f478
wil <wil.fabri777@gmail.com>
2026-06-01 11:45:16 -0400

    Validaciones y mensajes en DTOs de Producto
    Se actualizaron las anotaciones de validación en CreateProductoDto y UpdateProductoDto, añadiendo mensajes de error personalizados y ajustando los rangos de validación para Nombre, Precio y Stock. También se agregaron comentarios explicativos para mayor claridad en el código.

--- SistemaVentas.Application/DTOs/CreateProductoDto.cs
+++ SistemaVentas.Application/DTOs/CreateProductoDto.cs
@@ -5,13 +5,16 @@ namespace SistemaVentas.Application.DTOs;
 // DTO pare crear productos desde la API
 public class CreateProductoDto
 {
-    [Required]
-    [MaxLength(255)]
+    // Data Annotations para validar los datos de entrada
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [MaxLength(100, ErrorMessage = "El nombre no puede superar 100 caracteres")]
     public string Nombre { get; set; } = string.Empty;
 
-    [Range(0.01, 999999)]
+    // El precio debe ser mayor que cero
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio deber ser mayor que cero")]
     public decimal Precio { get; set; }
 
-    [Range(0,100000)]
+    // El stock no puede ser negativo
+    [Range(0,int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
     public int Stock { get; set; }
 }
--- SistemaVentas.Application/DTOs/UpdateProductoDto.cs
+++ SistemaVentas.Application/DTOs/UpdateProductoDto.cs
@@ -4,13 +4,16 @@ namespace SistemaVentas.Application.DTOs;
 
 public class UpdateProductoDto
 {
-    [Required]
-    [MaxLength(100)]
+    // Data Annotations para validar los datos de entrada
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [MaxLength(100, ErrorMessage = "El nombre no puede superar 100 caracteres")]
     public string Nombre { get; set; } = string.Empty;
 
-    [Range(0.01, 999999)]
+    // El precio debe ser mayor que cero
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio deber ser mayor que cero")]
     public decimal Precio { get; set; }
 
-    [Range(0, 1000)]
+    // El stock no puede ser negativo
+    [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
     public int Stock { get; set; }
 }
