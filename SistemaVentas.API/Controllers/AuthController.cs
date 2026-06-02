6cba19590959d60df103a233e2566e675adfc373
wil <wil.fabri777@gmail.com>
2026-06-01 21:38:19 -0400

    AuthController con endpoints de registro y login
    Se incorpora el controlador AuthController en SistemaVentas.API.Controllers, proporcionando endpoints para registrar usuarios y autenticarlos. Utiliza IAuthService y DTOs para gestionar la lógica de autenticación y las respuestas.

---
+++ SistemaVentas.API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVentas.Application.DTOs;
+using SistemaVentas.Application.Interfaces;
+
+namespace SistemaVentas.API.Controllers;
+
+// Controlador para gestionar la autenticacion de usuarios
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _service;
+
+    public AuthController(IAuthService service)
+    {
+        _service = service;
+    }
+
+    // Endpoint POST: /api/auth/register
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(CreateUsuarioDto dto)
+    {
+        await _service.RegisterAsync(dto);
+
+        return Ok("Usuario registrado exitosamente");
+    }
+
+    // Endpoint POST: /api/auth/login
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto dto)
+    {
+        var resultado = await _service.LoginAsync(dto);
+
+        return Ok(resultado);
+    }
+}
