c07892eff5277d86060f37d70ccc48e7858f127f
wil <wil.fabri777@gmail.com>
2026-06-01 22:05:03 -0400

    Generar y devolver JWT en el servicio de autenticación
    Se reemplaza el mensaje de login exitoso por la generación y retorno de un token JWT firmado. Se agregan claims, clave secreta y credenciales de firma. Se añaden referencias y el paquete JwtBearer para soporte de autenticación JWT.

--- SistemaVentas.Application/Services/AuthService.cs
+++ SistemaVentas.Application/Services/AuthService.cs
@@ -1,6 +1,10 @@
-﻿using SistemaVentas.Application.DTOs;
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using SistemaVentas.Application.DTOs;
 using SistemaVentas.Application.Interfaces;
 using SistemaVentas.Domain.Entities;
+using System.Security.Claims;
+using System.Text;
 
 namespace SistemaVentas.Application.Services;
 
@@ -34,6 +38,41 @@ public class AuthService : IAuthService
             throw new Exception("Credenciales inválidas");
         }
 
-        return "LOGIN_EXITOSO";
+        var claims = new[]
+        {
+            new Claim(
+                ClaimTypes.Name,
+                usuario.Nombre
+            ),
+
+            new Claim(
+                ClaimTypes.Email,
+                usuario.Email
+            )
+        };
+
+        var key =
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    "MiClaveSuperSecreta123456789"
+                )
+            );
+
+        var credentials =
+            new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256
+            );
+
+        var token = new JwtSecurityToken(
+            issuer: "SistemaVentas",
+            audience: "SistemaVentas",
+            claims: claims,
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler()
+            .WriteToken(token);
     }
 }
